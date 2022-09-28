9dbd1777fb74669a409fa0eb8c0fd84865c4aa53
NatasyaPrameswari <natasyaprameswari30@gmail.com>
2022-09-28 10:18:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/app_20753061_tugasintent/tentang.java
@@ -0,0 +1,26 @@
+package com.example.app_20753061_tugasintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+        Button kembali = findViewById(R.id.kembali);
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
+
